752464c1b471c9544707b84908a2dc580004e6d2
lucasabrantes1 <lucasabrantes002@gmail.com>
2025-04-18 15:11:49 -0300

    Salvando usuários em memória (Headers)

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -20,15 +20,25 @@ import http from 'node:http'
 // GET /users => Buscando usuários no banc-end
 // POST /users => Criar um usuário no back-end
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+    .setHeader('Content-type','application/json')
+    .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
-    return res.end('Criação de usuário')
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
+
+    return res.end("Criação de usuário")
   }
 
   return res.end('Hello World')
